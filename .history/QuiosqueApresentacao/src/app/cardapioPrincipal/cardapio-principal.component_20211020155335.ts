a7891c212e3d05d4cb309a582b2c3f4ddf6e9099
sergiofeitosa <sergiofeitosa@gmail.com>
2021-10-20 20:23:57 -0300

    importar imagens das refeiçoes e bebidas

---
+++ .history/QuiosqueApresentacao/src/app/cardapioPrincipal/cardapio-principal.component_20211020155335.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardapio-principal',
+  templateUrl: './cardapio-principal.component.html',
+})
+
+export class CardapioPrincipalComponent im {
+
+  // tslint:disable-next-line:no-inferrable-types
+  buttonDisabled: boolean;
+
+  element1: HTMLElement;
+  element2: HTMLElement;
+  element3: HTMLElement;
+  element4: HTMLElement;
+  element5: HTMLElement;
+  element6: HTMLElement;
+  element7: HTMLElement;
+  element8: HTMLElement;
+
+  constructor() {
+    this.buttonDisabled = false;
+  }
+
+  ngOnInit(): void {
+
+    if (! this.buttonDisabled ) {
+      console.log('carregando vazio');
+    }
+  }
+
+
+  // tslint:disable-next-line:typedef
+  updateClassDisabled() {
+    this.buttonDisabled = false;
+    console.log('passei23333 ' + this.buttonDisabled);
+
+    this.element1 = document.getElementById('desabilitado1') as HTMLElement;
+    this.element2 = document.getElementById('desabilitado2') as HTMLElement;
+    this.element3 = document.getElementById('desabilitado3') as HTMLElement;
+    this.element4 = document.getElementById('desabilitado4') as HTMLElement;
+    this.element5 = document.getElementById('desabilitado5') as HTMLElement;
+    this.element6 = document.getElementById('desabilitado6') as HTMLElement;
+    this.element7 = document.getElementById('desabilitado7') as HTMLElement;
+    this.element8 = document.getElementById('desabilitado8') as HTMLElement;
+
+    this.element1.removeAttribute('disabled');
+    this.element2.removeAttribute('disabled');
+    this.element3.removeAttribute('disabled');
+    this.element4.removeAttribute('disabled');
+    this.element5.removeAttribute('disabled');
+    this.element6.removeAttribute('disabled');
+    this.element7.removeAttribute('disabled');
+    this.element8.removeAttribute('disabled');
+  }
+}
