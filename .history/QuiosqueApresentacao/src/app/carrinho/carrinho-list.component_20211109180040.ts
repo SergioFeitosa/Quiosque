c4ba8490afdada05ccdc0b233a9fce54e6b662d3
sergiofeitosa <sergiofeitosa@gmail.com>
2021-11-09 19:05:24 -0300

    ajustes nos tamanhos dos campos

---
+++ .history/QuiosqueApresentacao/src/app/carrinho/carrinho-list.component_20211109180040.ts
@@ -0,0 +1,158 @@
+import { Produto } from '../produto/produto';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarrinhoService } from './carrinho.service';
+import { Carrinho } from './carrinho';
+import { Component, OnInit } from '@angular/core';
+import { Pedido } from '../pedido/pedido';
+import { PedidoService } from '../pedido/pedido.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  templateUrl: './carrinho-list.component.html',
+})
+
+export class CarrinhoListComponent implements OnInit {
+
+  Subscription
+  
+  statusText: string;
+  
+  // tslint:disable-next-line:variable-name
+  _categoryId: string;
+
+  filteredCarrinhos: Carrinho[] = [];
+  carrinhos: Carrinho[] = [];
+  // tslint:disable-next-line:variable-name
+  pedido = {} as Pedido;
+
+  produto = {} as Produto;
+
+  carrinho = {} as Carrinho;
+  // tslint:disable-next-line:variable-name
+  _carrinho = {} as Carrinho;
+
+  // tslint:disable-next-line:variable-name
+  _carrinhos: Carrinho[] = [];
+
+  // tslint:disable-next-line:variable-name
+  _filterBy: string;
+
+
+  constructor(
+    private carrinhoService: CarrinhoService,
+    private pedidoService: PedidoService,
+    private router: Router,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+
+    if (+environment.telefone === 99999999999 || +environment.telefone === 99999999997) {
+      this.carrinhoService.read().subscribe(carrinhos => {
+        this.carrinhos = carrinhos;
+        this.filteredCarrinhos = this.carrinhos
+          .filter((carrinho: Carrinho) => carrinho.enviadoPedido !== true);
+
+      });
+    } else {
+
+      this.carrinhoService.read().subscribe(carrinhos => {
+        this.carrinhos = carrinhos;
+        this.filteredCarrinhos = this.carrinhos
+          .filter((carrinho: Carrinho) => carrinho.telefone === environment.telefone)
+          .filter((carrinho: Carrinho) => carrinho.enviadoPedido !== true);
+
+      });
+    }
+  }
+
+  // tslint:disable-next-line:typedef
+  get filter() {
+    return this._filterBy;
+  }
+
+  set filter(value: string) {
+    this._filterBy = value;
+
+    if (+environment.telefone === 99999999999 || +environment.telefone === 99999999997) {
+
+      this.filteredCarrinhos =
+        this.carrinhos
+          .filter((carrinho: Carrinho) => carrinho.enviadoPedido !== true)
+          .filter((carrinho: Carrinho) => carrinho.produto.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+
+    } else {
+
+      this.filteredCarrinhos =
+        this.carrinhos
+          .filter((carrinho: Carrinho) => carrinho.enviadoPedido !== true)
+          .filter((carrinho: Carrinho) => carrinho.telefone === environment.telefone)
+          .filter((carrinho: Carrinho) => carrinho.produto.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+
+    }
+  }
+  // tslint:disable-next-line:quotemark
+  // tslint:disable-next-line:member-ordering
+  displayStyle = 'none';
+
+  // tslint:disable-next-line:typedef
+  openPopup(carrinhoId: number): void {
+    // tslint:disable-next-line:no-unused-expression
+    this.carrinhoService.readById(carrinhoId).subscribe(carrinho => {
+      this.carrinho = carrinho;
+      this.produto = this.carrinho.produto;
+    });
+
+    this.displayStyle = 'block';
+  }
+
+  // tslint:disable-next-line:typedef
+  closePopup() {
+    this.displayStyle = 'none';
+    // tslint:disable-next-line:prefer-const
+    let currentUrl = this.router.url;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([currentUrl]);
+
+  }
+
+
+  pedidoCreate(carrinhoId: number): void {
+
+    // tslint:disable-next-line:no-unused-expression
+    this.carrinhoService.readById(carrinhoId).subscribe(carrinho => {
+      this.carrinho = carrinho;
+
+      if (this.carrinho.enviadoPedido !== true) {
+
+        this.carrinho.enviadoPedido = true;
+        this.atualizarCarrinho(carrinho);
+
+
+        this.pedido.produto = this.carrinho.produto;
+        this.pedido.telefone = this.carrinho.telefone;
+        this.pedido.local = this.carrinho.local;
+        this.pedido.quantidade = this.carrinho.quantidade;
+        this.pedido.observacao = this.carrinho.observacao;
+        this.pedido.isencao = this.carrinho.isencao;
+        this.pedido.releaseDate = this.carrinho.releaseDate;
+        this.pedido.enviadoEntrega = false;
+
+        this.pedidoService.create(this.pedido).subscribe(() => {
+          this.pedidoService.showMessage('Pedido solicitado');
+        });
+      }
+    });
+  }
+
+
+  // tslint:disable-next-line:typedef
+  atualizarCarrinho(carrinho: Carrinho) {
+    this.carrinhoService.update(carrinho).subscribe(() => {
+      this.carrinhoService.showMessage('Carrinho Atualizado');
+      this.router.navigate(['/carrinho']);
+    });
+  }
+}
---
+++ .history/QuiosqueApresentacao/src/app/carrinho/carrinho.service_20211109160231.ts
@@ -0,0 +1,61 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Carrinho } from './carrinho';
+import { MatSnackBar} from '@angular/material/snack-bar';
+import { Subject } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class CarrinhoService {
+
+  // tslint:disable-next-line:quotemark
+  baseUrl = "http://localhost:3001/carrinhos";
+
+  constructor(private snackBar: MatSnackBar,
+              private http: HttpClient) {}
+
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, '', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: ['custom-css-class']
+    });
+  }
+
+  create(carrinho: Carrinho): Observable<Carrinho>{
+    return this.http.post<Carrinho>(this.baseUrl, carrinho);
+
+  }
+
+  update(carrinho: Carrinho): Observable<Carrinho>{
+    const url = `${this.baseUrl}/${carrinho.id}`;
+    return this.http.put<Carrinho>(url, carrinho);
+
+  }
+
+  read(): Observable<Carrinho[]> {
+    return this.http.get<Carrinho[]>(this.baseUrl);
+
+  }
+
+  readById(carrinhoId: number): Observable<Carrinho> {
+    const url = `${this.baseUrl}/${carrinhoId}`;
+    return this.http.get<Carrinho>(url);
+  }
+
+  delete(carrinhoId: number): Observable<Carrinho> {
+    const url = `${this.baseUrl}/${carrinhoId}`;
+    return this.http.delete<Carrinho>(url);
+  }
+
+  private _listners = new Subject<any>();
+  listen(): Observable<any> {
+    return 
+  }
+}
+
